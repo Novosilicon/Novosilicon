689b8d5a884ca2454b59612a5b13e800cc927771
jack.sqa <jack.sqa@hotmail.com>
2017-08-25 23:49:47 +0200

    Adding mising files

---
+++ Novosilicon/Core/BaseTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Novosilicon.Core
+{
+    public class BaseTest : Browser
+    {
+        [SetUp]
+        public void Initial() => Browser.InitBrowser("Chrome");
+
+        [OneTimeTearDown]
+        public void CloseBrowser()
+        {
+            Browser.CloseAllDrivers();
+        }
+
+    }
+}
--- Novosilicon/test.cs
+++ Novosilicon/test.cs
@@ -7,17 +7,24 @@ using OpenQA.Selenium;
 using NUnit.Framework;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Chrome;
+using Novosilicon.Core;
 
 namespace Novosilicon
 {
-    class FirstTest
+    class FirstTest : BaseTest
     {
         [Test]
         public void Test1()
         {
             System.Console.WriteLine("Test");
-            IWebDriver Driver = new ChromeDriver();
-            Driver.Url="http://www.google.com";
+            Browser.Driver.Url = "http://www.ya.ru";
+            
+        }
+        [Test]
+        public void Test2()
+        {
+            Browser.Driver.Url = "http://www.drom.ru";
+
         }
     }
 }
