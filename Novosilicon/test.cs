ea9e520569089eb224be6a0b7f94ab00af7c4904
jack.sqa <jack.sqa@hotmail.com>
2017-08-25 23:47:21 +0200

    First changes

---
+++ Novosilicon/test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using NUnit.Framework;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+
+namespace Novosilicon
+{
+    class FirstTest
+    {
+        [Test]
+        public void Test1()
+        {
+            System.Console.WriteLine("Test");
+            IWebDriver Driver = new ChromeDriver();
+            Driver.Url="http://www.google.com";
+        }
+    }
+}
