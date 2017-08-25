3734d02faac4dca8b3f6a582df8676de9570600f
jack.sqa <jack.sqa@hotmail.com>
2017-08-26 00:30:49 +0200

    Small refactoring

--- Novosilicon/TestCases/test.cs
+++ Novosilicon/TestCases/test.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using OpenQA.Selenium;
-using NUnit.Framework;
-using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.Chrome;
+﻿using NUnit.Framework;
 using Novosilicon.Core;
 
 namespace Novosilicon
